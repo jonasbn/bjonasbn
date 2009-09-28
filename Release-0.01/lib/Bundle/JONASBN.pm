e16bb530082a50405a4e96a764742cd29d8871d2
jonasbn <jonasbn@logiclab.dk>
2009-09-28 19:50:29 +0000

    Initial release
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/BJONASBN/tags@6618 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ Release-0.01/lib/Bundle/JONASBN.pm
@@ -0,0 +1,98 @@
+package Bundle::JONASBN;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Bundle::JONASBN - Collection of modules often used by jonasbn
+
+=head1 SYNOPSIS
+
+    perl -MCPAN -e 'install Bundle::JONASBN'
+
+=head1 CONTENTS
+
+=over
+
+=item * L<YAML>
+
+=item * L<Tie::IxHash>
+
+=item * L<XML::Simple>
+
+=item * L<Perl::Tidy>
+
+=item * L<ExtUtils::Install>, 1.47
+
+=item * L<Test::Pod>, 1.14
+
+=item * L<Test::Perl::Critic>
+
+=item * L<Error>
+
+=item * L<Module::Load>
+
+=item * L<File::Path>
+
+=item * L<Module::Build>, 0.35
+
+=item * L<Test::Kwalitee>
+
+=item * L<List::MoreUtils>
+
+=item * L<Module::Info::File>
+
+=item * L<DBI>
+
+=item * L<Test::Exception>
+
+=item * L<Test::Pod::Coverage>, 1.04
+
+=item * L<Hash::Merge>
+
+=item * L<Data::Dumper>
+
+=item * L<Template>
+
+=item * L<Date::Calc>
+
+=item * L<Test::More>
+
+=item * L<Spreadsheet::WriteExcel>
+
+=item * L<Params::Validate>, 0.85
+
+=item * L<AppConfig>
+
+=item * L<JSON>
+
+=item * L<Cwd>
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Bundle::COG>
+
+=back
+
+=head1 AUTHOR
+
+Jonas B. Nielsen (jonasbn), C<< <jonasbn@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Jonas B. Nielsen (jonasbn), All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
