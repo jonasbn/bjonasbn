04f75a4d53380e3b91bec982e4919b7a929985cc
jonasbn <jonasbn@logicLAB.dk>
2014-09-01 19:41:03 +0200

    Added CPAN, build and coveralls badges

--- lib/Task/BeLike/JONASBN.pm
+++ lib/Task/BeLike/JONASBN.pm
@@ -18,6 +18,14 @@ __END__
 
 =pod
 
+=begin markdown
+
+[![CPAN version](https://badge.fury.io/pl/Task::BeLike::JONASBN.svg)](http://badge.fury.io/pl/Task::BeLike::JONASBN)
+[![Build Status](https://travis-ci.org/jonasbn/bjonasbn.svg?branch=master)](https://travis-ci.org/jonasbn/bjonasbn)
+[![Coverage Status](https://coveralls.io/repos/jonasbn/bjonasbn/badge.png)](https://coveralls.io/r/jonasbn/bjonasbn)
+
+=end markdown
+
 =head1 NAME
 
 Task::BeLike::JONASBN - Collection of modules often used by jonasbn
